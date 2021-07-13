ecd2e6445f83a191ee0d3790f4d21a0d8268757f
Evelyn Hasama <ehasama11@gmail.com>
2021-07-13 12:57:39 -0700

    user can log out on profile fragment rebasing to resolve merge conflicts with MainActivity

--- app/src/main/java/com/example/bucketbuds/ProfileFragment.java
+++ app/src/main/java/com/example/bucketbuds/ProfileFragment.java
@@ -1,12 +1,17 @@
 package com.example.bucketbuds;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.parse.ParseUser;
 
 /**
  * A simple {@link Fragment} subclass.
