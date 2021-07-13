73370890119348b63af3b200ffd22d3eed41852c
Evelyn Hasama <ehasama11@gmail.com>
2021-07-13 15:20:44 -0700

    user can view their profile information

--- app/src/main/java/com/example/bucketbuds/ProfileFragment.java
+++ app/src/main/java/com/example/bucketbuds/ProfileFragment.java
@@ -4,67 +4,117 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.google.android.material.shape.RoundedCornerTreatment;
+import com.google.android.material.tabs.TabLayout;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.bumptech.glide.Glide;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String TAG = "ProfileFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View view;
+    File photoFile;
+    TextView tvUsername;
+    TextView tvFirst;
+    TextView tvLast;
+    TextView tvBio;
+    TextView tvFriendCount;
+    TextView tvBucketCount;
+    ImageView ivProfileImage;
+    Button btnEditProfile;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    User user;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
+    public static ProfileFragment newInstance() {
         ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        user = new User(ParseUser.getCurrentUser());
+        tvUsername = view.findViewById(R.id.tvUsernamePF);
+        tvFirst = view.findViewById(R.id.tvFirstNamePF);
+        tvLast = view.findViewById(R.id.tvLastNamePF);
+        tvBio = view.findViewById(R.id.tvBioPF);
+        tvFriendCount = view.findViewById(R.id.tvFriendCountPF);
+        tvBucketCount = view.findViewById(R.id.tvBucketCountPF);
+        ivProfileImage = view.findViewById(R.id.ivProfileImagePF);
+        btnEditProfile = view.findViewById(R.id.btnEditProfile);
+        tabLayout = view.findViewById(R.id.sliding_tabsPF);
+        viewPager = view.findViewById(R.id.viewpagerPF);
+
+        ParseFile image = user.getImage();
+        if (image == null){
+            Glide.with(view).load(R.drawable.profile_placeholder).transform(new CenterCrop(), new RoundedCorners(12)).into(ivProfileImage);
+        } else {
+            Glide.with(view).load(image.getUrl()).centerCrop().transform(new RoundedCorners(12)).into(ivProfileImage);
+        }
+        tvFirst.setText(user.getFirstName());
+        tvLast.setText(user.getLastName());
+        tvUsername.setText("@"+user.getUsername());
+        tvFriendCount.setText(String.valueOf(user.getFriendCount()));
+        tvBucketCount.setText(String.valueOf(user.getBucketCount()));
+
+        String bio = user.getBio();
+        if (bio == null) {
+            tvBio.setVisibility(View.INVISIBLE);
+        } else {
+            tvBio.setVisibility(View.VISIBLE);
+            tvBio.setText(bio);
+        }
+
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Edit Profile", Toast.LENGTH_LONG).show();
+                // TODO: Open dialog to edit profile
+            }
+        });
+
+        ivProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Change Profile Picture", Toast.LENGTH_LONG).show();
+                // TODO: Open camera to change profile picture
+            }
+        });
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/example/bucketbuds/User.java
@@ -0,0 +1,98 @@
+package com.example.bucketbuds;
+
+import com.parse.ParseFile;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+public class User{
+
+    private ParseUser user;
+
+    public User(ParseUser parseUser) {
+        user = parseUser;
+    }
+
+    public static final String KEY_FIRST_NAME = "firstName";
+    public static final String KEY_LAST_NAME = "lastName";
+    public static final String KEY_BIO = "bio";
+    public static final String KEY_FRIENDS = "friends";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_BUCKET_COUNT = "bucketCount";
+    public static final String KEY_FRIEND_COUNT = "friendCount";
+
+    public String getUsername(){
+        return user.getUsername();
+    }
+
+    public void setUsername(String username){
+        user.setEmail(username);
+    }
+
+    public String getEmail(){
+        return user.getEmail();
+    }
+
+    public void setEmail(String email){
+        user.setEmail(email);
+    }
+
+
+    public int getBucketCount() {
+        return user.getInt(KEY_BUCKET_COUNT);
+    }
+
+    public void setBucketCount(int bucketCount) {
+        user.put(KEY_BUCKET_COUNT, bucketCount);
+    }
+
+    public int getFriendCount() {
+        return user.getInt(KEY_FRIEND_COUNT);
+    }
+
+    public void setFriendCount(int friendCount) {
+        user.put(KEY_FRIEND_COUNT, friendCount);
+    }
+
+    public String getFirstName() {
+        return user.getString(KEY_FIRST_NAME);
+    }
+
+    public void setFirstName(String firstName) {
+        user.put(KEY_FIRST_NAME, firstName);
+    }
+
+    public String getLastName() {
+        return user.getString(KEY_LAST_NAME);
+    }
+
+    public void setLastName(String lastName) {
+        user.put(KEY_LAST_NAME, lastName);
+    }
+
+    public String getBio() {
+        return user.getString(KEY_BIO);
+    }
+
+    public void setBio(String bio) {
+        user.put(KEY_BIO, bio);
+    }
+
+    public ParseFile getImage() {
+        return user.getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile image) {
+        user.put(KEY_IMAGE, image);
+    }
+
+    public void addFriend(ParseUser friend) {
+        ParseRelation<ParseUser> friendsRelation = user.getRelation(KEY_FRIENDS);
+        friendsRelation.add(friend);
+    }
+
+    public void removeFriend(ParseUser friend) {
+        ParseRelation<ParseUser> friendsRelation = user.getRelation(KEY_FRIENDS);
+        friendsRelation.remove(friend);
+    }
+
+}
