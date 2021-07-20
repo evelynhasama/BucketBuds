a6b4ba30cf9d265a56817dee12cae757e5028d06
Evelyn Hasama <ehasama11@gmail.com>
2021-07-20 10:08:00 -0700

    users can view the bucket list's users

--- app/src/main/java/com/evelynhasama/bucketbuds/BucketActivitiesFragment.java
+++ app/src/main/java/com/evelynhasama/bucketbuds/BucketActivitiesFragment.java
@@ -3,15 +3,27 @@ package com.evelynhasama.bucketbuds;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BucketActivitiesFragment extends Fragment {
 
@@ -19,6 +31,8 @@ public class BucketActivitiesFragment extends Fragment {
 
     BucketList bucketList;
     View view;
+    List<User> users;
+    BucketUsersAdapter bucketUsersAdapter;
 
     public BucketActivitiesFragment() {
     }
@@ -45,16 +59,53 @@ public class BucketActivitiesFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_bucket_activities, container, false);
 
-        TextView tvCompleted = view.findViewById(R.id.tvCompletedFBA);
+        Switch swCompleted = view.findViewById(R.id.swCompletedFBA);
         TextView tvBucketName = view.findViewById(R.id.tvBucketNameFBA);
         TextView tvBucketDescription = view.findViewById(R.id.tvBucketDescriptionFBA);
         ImageView ivBucketImage = view.findViewById(R.id.ivBucketImageFBA);
+        Button btnEditBucket = view.findViewById(R.id.btnEditBucketFBA);
+        Button btnAddActivity = view.findViewById(R.id.btnAddActivityFBA);
+        RecyclerView rvBucketUsers = view.findViewById(R.id.rvBucketFriendsFBA);
+        RecyclerView rvBucketActivities = view.findViewById(R.id.rvActivitiesFBA);
 
-        Glide.with(getContext()).load(bucketList.getImage().getUrl()).centerCrop().into(ivBucketImage);
+        Glide.with(getContext()).load(bucketList.getImage().getUrl()).centerCrop()
+                .transform(new RoundedCorners(10)).into(ivBucketImage);
         tvBucketName.setText(bucketList.getName());
         tvBucketDescription.setText(bucketList.getDescription());
-        tvCompleted.setText(bucketList.getCompleted() ? "Completed" : "Active");
+        swCompleted.setChecked(bucketList.getCompleted() ? true : false);
 
+        btnAddActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: open dialog to create activity
+            }
+        });
+
+        btnEditBucket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: open dialog to edit
+            }
+        });
+
+        users = new ArrayList<>();
+        bucketUsersAdapter = new BucketUsersAdapter(getContext(), users);
+        rvBucketUsers.setAdapter(bucketUsersAdapter);
+        rvBucketUsers.setLayoutManager(new LinearLayoutManager(getContext(),
+                RecyclerView.HORIZONTAL, false));
+
+        bucketList.getUsersRelation().getQuery().findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                for (ParseUser user: objects){
+                    users.add(new User(user));
+                    bucketUsersAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        // TODO: recycler view activities
+        
         return view;
     }
 
---
+++ app/src/main/java/com/evelynhasama/bucketbuds/BucketUsersAdapter.java
@@ -0,0 +1,77 @@
+package com.evelynhasama.bucketbuds;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+public class BucketUsersAdapter extends RecyclerView.Adapter<BucketUsersAdapter.ViewHolder> {
+
+    public static final String TAG = "BucketUsersAdapter";
+    Context context;
+    List<User> users;
+
+    public BucketUsersAdapter(Context context, List<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    public void clear() {
+        users.clear();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder");
+        View view = LayoutInflater.from(context).inflate(R.layout.item_bucket_friend_circle, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.bind(user);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivProfileImage;
+        TextView tvFirstName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImageIBFC);
+            tvFirstName = itemView.findViewById(R.id.tvFirstNameIBFC);
+        }
+
+        public void bind(User user) {
+            if (user.getImage() != null) {
+                Glide.with(context).load(user.getImage().getUrl()).circleCrop().into(ivProfileImage);
+            } else {
+                Glide.with(context).load(R.drawable.profile_placeholder).circleCrop().into(ivProfileImage);
+            }
+            tvFirstName.setText(user.getFirstName());
+        }
+    }
+
+}
+
